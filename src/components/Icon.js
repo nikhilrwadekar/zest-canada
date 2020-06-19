d3a25d3910050ba8628fead54ddf7be192c804e7
Nikhil Wadekar <nikhilrwadekar@gmail.com>
2020-06-19 03:02:03 -0700

    Added Icon Assets; Fixed Styling

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from "react";
 
-export default function Icon() {
-	return (
-		<div>
-			
-		</div>
-	)
+export default function Icon({ imageSrc, caption }) {
+  return (
+    <span style={{ padding: "1rem", margin: "1rem" }}>
+      <img src={imageSrc} style={{ width: 200, height: 200 }} />
+      <h3>{caption}</h3>
+    </span>
+  );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,25 @@
 import React from "react";
+import Icon from "../components/Icon";
 export default function Home() {
   return (
     <>
       <h1>ZEST</h1>
       <h2>Copy & Print</h2>
+
+      <div className="Icons">
+        <Icon
+          imageSrc={process.env.PUBLIC_URL + "/icons/print.svg"}
+          caption="Print it."
+        />
+        <Icon
+          imageSrc={process.env.PUBLIC_URL + "/icons/box.svg"}
+          caption="Pack it."
+        />
+        <Icon
+          imageSrc={process.env.PUBLIC_URL + "/icons/delivery-truck.svg"}
+          caption="Ship it."
+        />
+      </div>
     </>
   );
 }
